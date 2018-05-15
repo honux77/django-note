13e26eed5eb22ad6b7887710c2de112c9a7f4c12
Hoyoung Jung <honux77@gmail.com>
2018-05-16 01:10:39 +0900

    Update vs code settings

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('simple.urls')),
+    path('', include('simple.urls')), #main app
 ]
